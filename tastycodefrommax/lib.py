9217b2cdf5fa4d488dad666d2ecb2336f1cb0849
Max Hoechsmann <max.hoechsmann@googlemail.com>
2022-05-23 16:25:15 +0200

    Weißwurst for everyone

---
+++ tastycodefrommax/lib.py
@@ -0,0 +1,4 @@
+def try_me():
+    user_name = input("What´s your name?")
+    sausage = input ("What´s your favourite Bavarian sausage?")
+    print("Hi, I am " + user_name + " and my favourite Bavarian sausage is "+ sausage + " from Le Wagon!")
